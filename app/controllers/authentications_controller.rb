0fc405e6be788c76393c4813114ce3f244ca0464
Student <example@example.com>
2020-01-29 14:51:56 -0800

    add changes to authentications controller and index

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,14 +1,19 @@
 class AuthenticationsController < ApplicationController
 
   def index
-    # @authentication = Authentication.new
-
+    @product = Product.new
     render :index
+  end
+
+
+  def create
+    # @authentication = Authentication.new(authentication_params)
+    HTTParty.post('HTTP://localhost:3000/authentications', {email:"example@mail.com",password:"123123123"})
+    # API STUFF
   end
 
 end
 
-def create
-  @authentication = Authentication.new(authentication_params)
-  # API STUFF
-end
+
+# $ curl -H "Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE0NjA2NTgxODZ9.xsSwcPC22IR71OBv6bU_OGCSyfE89DvEzWfDU0iybMA" http://localhost:3000/items
+# []
